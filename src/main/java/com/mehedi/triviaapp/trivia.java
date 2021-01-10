b1daddd66d007ec86ac70f4d29ceb1dc4de816e1
Monster2299 <atmehedihassan29@gmail.com>
2021-01-10 18:44:20 +0530

    progress update!

--- src/main/java/com/mehedi/triviaapp/trivia.java
+++ src/main/java/com/mehedi/triviaapp/trivia.java
@@ -3,8 +3,10 @@ package com.mehedi.triviaapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -17,6 +19,8 @@ import android.widget.Toast;
 import com.mehedi.triviaapp.data.AnswerListAsyncResponse;
 import com.mehedi.triviaapp.data.QuestionBank;
 import com.mehedi.triviaapp.model.Questions;
+import com.mehedi.triviaapp.model.Score;
+import com.mehedi.triviaapp.util.Prefs;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,11 +36,18 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
     private TextView count;
     private int currentQuestionIndex = 0;
     private List<Questions> questionsList;
+    private TextView scoreView;
+    private int scoreCount = 0;
+    private Prefs prefs;
+
+    private Score score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trivia);
+
+        score = new Score();
 
         txtques = findViewById(R.id.ques);
         imgprev = findViewById(R.id.imgprev);
@@ -45,24 +56,28 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
         btnwrong = findViewById(R.id.btnwrong);
         count = findViewById(R.id.count);
         card = findViewById(R.id.cardview);
+        scoreView = findViewById(R.id.highscore);
+        prefs = new Prefs(trivia.this);
 
         imgnext.setOnClickListener(this);
         imgprev.setOnClickListener(this);
         btnright.setOnClickListener(this);
         btnwrong.setOnClickListener(this);
 
+
+
+                Log.d("high_score", "onClick: "+prefs.getHighScore());
+
+        scoreView.setText("Current Score :"+String.valueOf(score.getScore()));
+
         questionsList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Questions> questionsArrayList) {
 
                 txtques.setText(questionsArrayList.get(currentQuestionIndex).getAnswer());
-               // Log.d("test", "processFinished: "+questionsArrayList);
 
             }
         });
-
-        //Log.d("main", "onCreate: "+questionsList);
-
 
     }
 
@@ -76,12 +91,13 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
                 }
                 break;
             case R.id.imgnext :
-               currentQuestionIndex = (currentQuestionIndex + 1) % questionsList.size();
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionsList.size();
                updateQuestion();
                 break;
             case R.id.btnright :
                 questionTrue(true);
                 updateQuestion();
+
                 break;
             case R.id.btnwrong :
                 questionTrue(false);
@@ -95,19 +111,46 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
         int toastmsg;
         if (userChoice == correctAns){
             fadeAnimation();
+            addScore();
             toastmsg = R.string.correct;
         }
         else {
             toastmsg=R.string.Wrong;
+            deductScore();
             shakeAnimation();
         }
         Toast.makeText(getApplicationContext(),toastmsg,Toast.LENGTH_SHORT).show();
+
     }
 
     private void updateQuestion() {
             String question = questionsList.get(currentQuestionIndex).getAnswer();
             txtques.setText(question);
             count.setText(currentQuestionIndex+1+"/"+questionsList.size());
+
+    }
+    private void addScore() {
+        scoreCount +=100;
+
+        score.setScore(scoreCount);
+        scoreView.setText("Current Score :"+String.valueOf(score.getScore()));
+        Log.d("score", "addScore: "+score.getScore());
+    }
+
+    private void deductScore() {
+        scoreCount -=100;
+        if (scoreCount >0){
+            score.setScore(scoreCount);
+            scoreView.setText("Current Score :"+String.valueOf(score.getScore()));
+            Log.d("score", "DeductScore: "+score.getScore());
+        }
+        else {
+            scoreCount = 0;
+            score.setScore(scoreCount);
+            scoreView.setText("Current Score :"+String.valueOf(score.getScore()));
+            Log.d("score", "DeductScore: "+score.getScore());
+        }
+
     }
     private void fadeAnimation(){
         AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,0.0f);
@@ -122,6 +165,7 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
             public void onAnimationStart(Animation animation) {
                 card.setCardBackgroundColor(Color.GREEN);
                 txtques.setTextColor(Color.YELLOW);
+
             }
 
             @Override
@@ -160,5 +204,11 @@ public class trivia extends AppCompatActivity implements View.OnClickListener {
 
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        prefs.saveHighScore(scoreCount);
+        super.onPause();
     }
 }
